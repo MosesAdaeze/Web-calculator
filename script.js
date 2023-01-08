b207d0b068ea5e1abe5f9a16946ff0a5ca7743cb
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-01-08 17:32:44 +0100

    more symbol

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function calculate(){
         (outputScreen.value);
     }
     catch(err){
-        outputScreen.value("invaojiad")
+        alert("Invalid")
     }
 }
 function wipe(){
@@ -21,6 +21,57 @@ function del(){
     .slice(0, -1)
 }
 
+// for wipe empty "" 
+// slice all button del to delete only a number ,-1
+
+function sin() {
+    screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+    screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+    screen.value = Math.tan(screen.value);
+}
+
+function pow() {
+    screen.value = Math.pow(screen.value, 2);
+}
+
+function sqrt() {
+    screen.value = Math.sqrt(screen.value, 2);
+}
+
+function log() {
+    screen.value = Math.log(screen.value);
+}
+
+function pi() {
+    screen.value = 3.14159265359;
+}
+
+function e() {
+    screen.value = 2.71828182846;
+}
+
+function fact() {
+    var i, num, f;
+    f = 1
+    num = screen.value;
+    for (i = 1; i <= num; i++) {
+        f = f * i;
+    }
+
+    i = i - 1;
+
+    screen.value = f;
+}
+
+function backspc() {
+    screen.value = screen.value.substr(0, screen.value.length - 1);
+}
 
 // NOTE: 1
 
